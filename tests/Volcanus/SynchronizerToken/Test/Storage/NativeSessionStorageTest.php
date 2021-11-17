5718536bcbc3b76cf179bab5011a440e289ce802
k_horii <k_horii@rikcorp.jp>
2021-11-17 15:34:56 +0900

    リファクタリング

--- tests/Volcanus/SynchronizerToken/Test/Storage/NativeSessionStorageTest.php
+++ tests/Volcanus/SynchronizerToken/Test/Storage/NativeSessionStorageTest.php
@@ -8,6 +8,9 @@
 
 namespace Volcanus\SynchronizerToken\Test\Storage;
 
+use Volcanus\SynchronizerToken\Storage\NativeSessionStorage;
+use Volcanus\SynchronizerToken\Storage\StorageInterface;
+
 /**
  * Test for NativeSessionStorage
  *
@@ -24,28 +27,28 @@ class NativeSessionStorageTest extends \PHPUnit\Framework\TestCase
 	public function testImplementsStorageInterface()
 	{
 		$attributes = array('foo', 'bar', 'baz');
-		$storage = new \Volcanus\SynchronizerToken\Storage\NativeSessionStorage('storageName', $attributes);
-		$this->assertInstanceOf('\Volcanus\SynchronizerToken\Storage\StorageInterface', $storage);
+		$storage = new NativeSessionStorage('storageName', $attributes);
+		$this->assertInstanceOf(StorageInterface::class, $storage);
 	}
 
 	public function testConstructorWithAttributes()
 	{
 		$attributes = array('foo', 'bar', 'baz');
-		$storage = new \Volcanus\SynchronizerToken\Storage\NativeSessionStorage('storageName', $attributes);
+		$storage = new NativeSessionStorage('storageName', $attributes);
 		$this->assertEquals($attributes, $storage->getAttributes());
 	}
 
 	public function testConstructorImportAttributesFromSession()
 	{
 		$_SESSION['storageName'] = array('foo', 'bar', 'baz');
-		$storage = new \Volcanus\SynchronizerToken\Storage\NativeSessionStorage('storageName');
+		$storage = new NativeSessionStorage('storageName');
 		$this->assertEquals($_SESSION['storageName'], $storage->getAttributes());
 	}
 
 	public function testSaveToSession()
 	{
 		$attributes = array('foo', 'bar', 'baz');
-		$storage = new \Volcanus\SynchronizerToken\Storage\NativeSessionStorage('storageName', $attributes);
+		$storage = new NativeSessionStorage('storageName', $attributes);
 		$storage->save();
 		$this->assertEquals($_SESSION['storageName'], $storage->getAttributes());
 	}
@@ -53,7 +56,7 @@ class NativeSessionStorageTest extends \PHPUnit\Framework\TestCase
 	public function testSaveToSessionWithAttributes()
 	{
 		$attributes = array('foo', 'bar', 'baz');
-		$storage = new \Volcanus\SynchronizerToken\Storage\NativeSessionStorage('storageName');
+		$storage = new NativeSessionStorage('storageName');
 		$storage->save($attributes);
 		$this->assertEquals($attributes, $storage->getAttributes());
 		$this->assertEquals($attributes, $_SESSION['storageName']);
